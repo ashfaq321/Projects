e56edd974b7c60f59494f8fcd7a6056bf70505a2
Muhammad Ashfaq <ashfaq441997@gmail.com>
2017-08-25 12:31:02 +0500

    Radio Group + menu list and its click listner

--- EpicApps/app/src/main/java/com/example/missionimopossible/epicapps/MainActivity.java
+++ EpicApps/app/src/main/java/com/example/missionimopossible/epicapps/MainActivity.java
@@ -2,16 +2,42 @@ package com.example.missionimopossible.epicapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethod;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+    Animation alphaLogin;
+    EditText passwordEditText;
+    CheckBox checkBoxId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("onCreate()","Created");
+        alphaLogin=AnimationUtils.loadAnimation(MainActivity.this,R.anim.alpha_anim_login);
+        passwordEditText=(EditText)findViewById(R.id.password_edittext);
+        checkBoxId=(CheckBox)findViewById(R.id.chck_box_id);
+        checkBoxId.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    passwordEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+                else{
+                   passwordEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }
+            }
+        });
     }
 
     @Override
--- ListView/app/src/main/java/com/example/jackreacher/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/jackreacher/listview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jackreacher.listview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -14,23 +15,24 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     ListView citiesBeautifulWorld;
     ArrayAdapter citiesBeautifulWorldArrayAdapter;
-    String[] citiesWorld={"Islamabad","Melbourn","Sydney","Alice Springs","London","Rome","Florence","Paris","Vience","Amsterdam"
-            ,"San Fransisco","Las Vegas","Istambol"};
+    String[] citiesWorld = {"Islamabad", "Melbourn", "Sydney", "Alice Springs", "London", "Rome", "Florence", "Paris", "Vience", "Amsterdam"
+            , "San Fransisco", "Las Vegas", "Istambol"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        citiesBeautifulWorld=(ListView)findViewById(R.id.cities_beautiful_world);
-        citiesBeautifulWorldArrayAdapter=new ArrayAdapter(MainActivity.this,R.layout.dummy_row_style,citiesWorld);
+        citiesBeautifulWorld = (ListView) findViewById(R.id.cities_beautiful_world);
+        citiesBeautifulWorldArrayAdapter = new ArrayAdapter(MainActivity.this, R.layout.dummy_row_style, citiesWorld);
         citiesBeautifulWorld.setAdapter(citiesBeautifulWorldArrayAdapter);
 
         citiesBeautifulWorld.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 // Getting text out from TextView
-                TextView mTitleText=(TextView)view;
-                String citiesName=mTitleText.getText().toString();
-                Toast.makeText(MainActivity.this, citiesName , Toast.LENGTH_SHORT).show();
+                TextView mTitleText = (TextView) view;
+                String citiesName = mTitleText.getText().toString();
+                Toast.makeText(MainActivity.this, citiesName, Toast.LENGTH_SHORT).show();
 
 
             }
@@ -41,8 +43,59 @@ public class MainActivity extends AppCompatActivity {
     /*Rule no 3: in method super statement should be first statement,and return should be the last.*/
 
     @Override
+    /**/
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.basic_menu_test,menu);
+        getMenuInflater().inflate(R.menu.basic_menu_test, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        /*Using Switch statement*/
+        switch (id) {
+
+            case R.id.opt_menu_refresh:
+                Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opt_menu_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opt_menu_select:
+                Toast.makeText(this, "Select", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opt_menu_edit:
+                Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opt_menu_delete:
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.opt_menu_info:
+                Toast.makeText(this, "Information", Toast.LENGTH_SHORT).show();
+                break;
+        }
+         /*Using if else Statement*/
+        /*if(id == R.id.opt_menu_refresh)
+        {
+            Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
+        }
+        else if(id==R.id.opt_menu_search)
+        {
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.opt_menu_select)
+        {
+            Toast.makeText(this, "Select", Toast.LENGTH_SHORT).show();
+        }else if (id==R.id.opt_menu_edit)
+        {
+            Toast.makeText(this, "Edit", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.opt_menu_delete)
+        {
+            Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.opt_menu_info)
+        {
+            Toast.makeText(this, "Information", Toast.LENGTH_SHORT).show();
+        }*/
+
         return true;
     }
 }
